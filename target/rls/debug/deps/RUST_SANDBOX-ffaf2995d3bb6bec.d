80a78a5a04c8238d0eb526bffb75f85a049371a3
Inferno-P <adamyatripathi@gmail.com>
2021-12-05 19:56:31 +0100

    Adding the src folder.

--- target/rls/debug/deps/RUST_SANDBOX-ffaf2995d3bb6bec.d
+++ target/rls/debug/deps/RUST_SANDBOX-ffaf2995d3bb6bec.d
@@ -1,6 +1,6 @@
-/home/adamya/SOL/RUST_SANDBOX/target/rls/debug/deps/RUST_SANDBOX-ffaf2995d3bb6bec.rmeta: src/main.rs src/types.rs src/strings.rs
+/home/adamya/SOL/RUST_SANDBOX/intro2rust/target/rls/debug/deps/RUST_SANDBOX-ffaf2995d3bb6bec.rmeta: src/main.rs src/types.rs src/strings.rs
 
-/home/adamya/SOL/RUST_SANDBOX/target/rls/debug/deps/RUST_SANDBOX-ffaf2995d3bb6bec.d: src/main.rs src/types.rs src/strings.rs
+/home/adamya/SOL/RUST_SANDBOX/intro2rust/target/rls/debug/deps/RUST_SANDBOX-ffaf2995d3bb6bec.d: src/main.rs src/types.rs src/strings.rs
 
 src/main.rs:
 src/types.rs:
